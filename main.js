6648fb0ba68c89fb7006ee2001456120ad089bb0
Diego Casallas <master.drcd@gmail.com>
2023-02-09 11:24:41 -0500

    added footer+name, funtion copy & footer styles

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-/*Escucha de eventos  */
-
-
 /** 
  * Requisitos:
 - Debe funcionar solo con letras minúsculas
@@ -9,12 +6,29 @@
 versión encriptada también devolver una palabra encriptada para su versión original.
 */
 
+/*acceso a elementos html */
+const textInput = document.getElementById('input-text');
+const btnEncrypt = document.querySelector('.button-encrypt');
+const btnDecrypt = document.querySelector('.button-decrypt');
+const btnCopy = document.querySelector('.button-copy');
+const textOutput = document.querySelector('.output__text');
+
+/*escucha de eventos */
+btnEncrypt.addEventListener('click', encrypt);
+btnDecrypt.addEventListener('click', decrypt);
+btnCopy.addEventListener('click', copy);
+
 /*Fucnion para copiar contenido */
+function copy() {
+ let textToCopy=textOutput.value;
+ textInput.value=textToCopy;
+ console.log(`Texto copiado: ${textInput.value}`);
+
+}
 /*Fucncion para comprobar que sea minuscula y no tenga acentos */
-
-
 /*Funcion para encriptar */
-function encrypt(palabra) {
+function encrypt() {
+    let palabra = textInput.value;
     const vocals = ['a', 'e', 'i', 'o', 'u'];
     const wordsToEncript = ["ai", "enter", "imes", "ober", "ufat"];
     let encryptWord = "";
@@ -33,16 +47,19 @@ function encrypt(palabra) {
             encryptWord += palabra[i];
         }
     }
+    console.log(encryptWord);
     return encryptWord
 }
 
 /* replace()para reemplazar*/
 /*Funcion para des-ecriptar */
-function decrypt(palabra) {
+function decrypt() {
+    let palabra = textInput.value;
     let decryptWord = palabra.replace(/ai/g, 'a')
         .replace(/enter/g, 'e')
         .replace(/imes/g, 'i')
         .replace(/ober/g, 'o')
         .replace(/ufat/g, 'u');
+    console.log(decryptWord);
     return decryptWord;
 }
